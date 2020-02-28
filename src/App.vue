83b37876a2e9ef86954e6293082ceb13785a56d5
alrafi <hafisalrafi@gmail.com>
2020-02-28 15:31:06 +0700

    add endpoint call to youtube api with axios

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,42 @@
 <template>
   <div>
-    <SearchBar></SearchBar>
+    <SearchBar @termChange="onTermChange"></SearchBar>
+    <VideoList></VideoList>
+    {{ videos.length }}
   </div>
 </template>
 
 <script>
-import SearchBar from "./components/SearchBar";
+import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
+import axios from 'axios';
+const API_KEY = 'AIzaSyBCiL5-MFK3JhSA69kGRvJT8oN4mbLGmjs  ';
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
-    SearchBar
+    SearchBar,
+    VideoList
+  },
+  data() {
+    return { videos: [] };
+  },
+  methods: {
+    onTermChange(searchTerm) {
+      // console.log(searchTerm);
+      axios
+        .get('https://www.googleapis/youtube/v3/search', {
+          params: {
+            key: API_KEY,
+            type: 'video',
+            part: 'snippet',
+            q: searchTerm
+          }
+        })
+        .then(res => {
+          this.videos = res.data.items;
+        });
+    }
   }
 };
 </script>
