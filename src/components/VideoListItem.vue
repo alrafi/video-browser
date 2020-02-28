0c3a761c63d1f157d017f730226471761559c194
alrafi <hafisalrafi@gmail.com>
2020-02-28 17:07:54 +0700

    add VideoListItem component

---
+++ src/components/VideoListItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <li class="list-group-item">
+    Video List Item
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'VideoListItem',
+  props: ['video']
+};
+</script>
+
+<style></style>
