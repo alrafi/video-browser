d7cbd8e09a071821f774187ed4e5779967ab8b0c
alrafi <hafisalrafi@gmail.com>
2020-03-01 02:17:31 +0700

    add iframe to show the video player

--- src/components/VideoDetail.vue
+++ src/components/VideoDetail.vue
@@ -1,13 +1,33 @@
 <template>
-  <div v-if="video">{{ video.snippet.title}}</div>
+  <div v-if="video" class="col-md-8">
+    <div class="embed-responsive embed-responsive-16by9">
+      <iframe class="embed-responsive-item" :src="videoUrl" />
+    </div>
+    <div class="details">
+      <h4>{{ video.snippet.title}}</h4>
+      <p>{{video.snippet.description}}</p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "VideoDetail",
-  props: ["video"]
+  props: ["video"],
+  computed: {
+    videoUrl() {
+      const videoId = this.video.id.videoId;
+      return `https://youtube.com/embed/${videoId}`;
+    }
+  }
 };
 </script>
 
-<style>
+<style scoped>
+.details {
+  margin-top: 10px;
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+}
 </style>
