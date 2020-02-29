61efb1b2cba170c2a6e5003dc3258d3d4e380072
alrafi <hafisalrafi@gmail.com>
2020-02-29 22:40:59 +0700

    add title and thumbnail to the list items

--- src/components/VideoListItem.vue
+++ src/components/VideoListItem.vue
@@ -1,13 +1,14 @@
 <template>
   <li class="list-group-item">
-    Video List Item
+    <img :src="video.snippet.thumbnails.default.url" />
+    {{ video.snippet.title }}
   </li>
 </template>
 
 <script>
 export default {
-  name: 'VideoListItem',
-  props: ['video']
+  name: "VideoListItem",
+  props: ["video"]
 };
 </script>
 
