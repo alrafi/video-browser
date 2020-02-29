78fd8719f5389bc84f96d0c62a7bbd9513bb3596
alrafi <hafisalrafi@gmail.com>
2020-02-29 23:20:26 +0700

    add api key to env

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,6 @@
 import SearchBar from "./components/SearchBar";
 import VideoList from "./components/VideoList";
 import axios from "axios";
-const API_KEY = "AIzaSyBCiL5-MFK3JhSA69kGRvJT8oN4mbLGmjs";
 
 export default {
   name: "App",
@@ -22,18 +21,16 @@ export default {
   },
   methods: {
     onTermChange(searchTerm) {
-      console.log(searchTerm);
       axios
         .get("https://www.googleapis.com/youtube/v3/search", {
           params: {
-            key: API_KEY,
+            key: process.env.VUE_APP_API_KEY,
             part: "snippet",
             q: searchTerm
           }
         })
         .then(res => {
           this.videos = res.data.items;
-          console.log(res);
         });
     }
   }
