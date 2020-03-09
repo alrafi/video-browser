6061b8b26eaa5dad9a159c08d23027fd880e5034
alrafi <hafisalrafi@gmail.com>
2020-03-09 12:57:28 +0700

    add SearchResultItem component

---
+++ src/components/SearchResultItem.vue
@@ -0,0 +1,57 @@
+<template>
+  <li>
+    <div class="image__container">
+      <img src="../assets/thumbnail.webp" alt="thumbnail" />
+    </div>
+    <div class="info__content">
+      <h3>lofi hip hop radio - beats to relax/study to</h3>
+      <p>chilledcow music</p>
+      <p>
+        Thank you for listening, I hope you will have a good time here :) Listen
+        to the playlist on Spotify, Apple music and more
+      </p>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'SearchResultItem'
+};
+</script>
+
+<style>
+/* search result */
+.search__result {
+  margin-top: 30px;
+}
+
+.search__result ul {
+  list-style: none;
+}
+
+.search__result li {
+  display: flex;
+  width: 80%;
+  margin-bottom: 15px;
+}
+
+.info__content h3 {
+  font-size: 120%;
+  margin-bottom: 5px;
+}
+
+.info__content p {
+  font-size: 70%;
+  margin-bottom: 5px;
+}
+
+.image__container {
+  width: 200px;
+  margin-right: 15px;
+}
+
+.image__container img {
+  width: 100%;
+}
+</style>
