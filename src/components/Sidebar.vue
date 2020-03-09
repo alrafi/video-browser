cdcebb56ca06b79a816e2c20f5769bc0387affa9
alrafi <hafisalrafi@gmail.com>
2020-03-09 15:47:42 +0700

    add router-link to title

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -2,64 +2,38 @@
   <div class="col-2 full__height sidebar">
     <div class="title">
       <i class="fas fa-bars"></i>
-      <img src="../assets/logo.png" alt="vue-logo" />
-      <h1>ueTube</h1>
+      <router-link to="/" class="title__app">
+        <img src="../assets/logo.png" alt="vue-logo" />
+        <h1>ueTube</h1>
+      </router-link>
     </div>
     <div class="menu__sidebar">
       <p>Menu</p>
       <ul>
-        <li>
-          <i class="fas fa-home"></i>Home
-        </li>
-        <li>
-          <i class="fas fa-fire"></i>Trending
-        </li>
-        <li>
-          <i class="fab fa-youtube"></i>Live
-        </li>
+        <li><i class="fas fa-home"></i>Home</li>
+        <li><i class="fas fa-fire"></i>Trending</li>
+        <li><i class="fab fa-youtube"></i>Live</li>
       </ul>
     </div>
     <div class="menu__sidebar">
       <p>Library</p>
       <ul>
-        <li>
-          <i class="fas fa-heart"></i>Loved
-        </li>
-        <li>
-          <i class="fas fa-bookmark"></i>Saved
-        </li>
+        <li><i class="fas fa-heart"></i>Loved</li>
+        <li><i class="fas fa-bookmark"></i>Saved</li>
       </ul>
     </div>
     <div class="menu__sidebar">
       <p>Category</p>
       <ul>
-        <li>
-          <i class="fas fa-music"></i>Music
-        </li>
-        <li>
-          <i class="fas fa-gamepad"></i>Gaming
-        </li>
-        <li>
-          <i class="fas fa-plane"></i>Travel & Events
-        </li>
-        <li>
-          <i class="fas fa-football-ball"></i>Sports
-        </li>
-        <li>
-          <i class="fas fa-camera"></i>Entertainment
-        </li>
-        <li>
-          <i class="fas fa-video"></i>Film & Animation
-        </li>
-        <li>
-          <i class="fas fa-car"></i>Autos & Vehicles
-        </li>
-        <li>
-          <i class="fas fa-book-open"></i>News & Politics
-        </li>
-        <li>
-          <i class="fas fa-dna"></i>Science & Tech
-        </li>
+        <li><i class="fas fa-music"></i>Music</li>
+        <li><i class="fas fa-gamepad"></i>Gaming</li>
+        <li><i class="fas fa-plane"></i>Travel & Events</li>
+        <li><i class="fas fa-football-ball"></i>Sports</li>
+        <li><i class="fas fa-camera"></i>Entertainment</li>
+        <li><i class="fas fa-video"></i>Film & Animation</li>
+        <li><i class="fas fa-car"></i>Autos & Vehicles</li>
+        <li><i class="fas fa-book-open"></i>News & Politics</li>
+        <li><i class="fas fa-dna"></i>Science & Tech</li>
       </ul>
     </div>
   </div>
@@ -67,7 +41,7 @@
 
 <script>
 export default {
-  name: "Sidebar"
+  name: 'Sidebar'
 };
 </script>
 
@@ -96,6 +70,21 @@ h1 {
   height: 14px;
   margin-right: 15px;
   margin-top: 3px;
+}
+
+.title a {
+  text-decoration: none;
+}
+
+.title a:link,
+.title a:active,
+.title a:hover,
+.title a:visited {
+  color: #d2cdd3;
+}
+
+.title__app {
+  display: flex;
 }
 
 .full__height {
