600a5c7a3cc14340f0e896ad8307e610339b0c82
alrafi <hafisalrafi@gmail.com>
2020-03-09 22:58:09 +0700

    show gamingItem with v-for

--- src/components/GamingContent.vue
+++ src/components/GamingContent.vue
@@ -3,34 +3,34 @@
     <div class="gaming__content">
       <p>Gaming</p>
       <ul>
-        <li class="item__gaming">
-          <img src="../assets/thumbnail.webp" alt="thumbnail" />
-          <h3>lofi hip hop radio - beats to relax/study to</h3>
-        </li>
-        <li class="item__gaming">
-          <img src="../assets/thumbnail.webp" alt="thumbnail" />
-          <h3>lofi hip hop radio - beats to relax/study to</h3>
-        </li>
-        <li class="item__gaming">
-          <img src="../assets/thumbnail.webp" alt="thumbnail" />
-          <h3>lofi hip hop radio - beats to relax/study to</h3>
-        </li>
-        <li class="item__gaming">
-          <img src="../assets/thumbnail.webp" alt="thumbnail" />
-          <h3>lofi hip hop radio - beats to relax/study to</h3>
-        </li>
-        <li class="item__gaming">
-          <img src="../assets/thumbnail.webp" alt="thumbnail" />
-          <h3>lofi hip hop radio - beats to relax/study to</h3>
-        </li>
+        <GamingItem
+          v-for="gaming in gamingVideos"
+          :key="gaming.etag"
+          :video="gaming"
+        ></GamingItem>
       </ul>
     </div>
   </div>
 </template>
 
 <script>
+import GamingItem from './GamingItem';
+import { mapGetters, mapActions } from 'vuex';
+
 export default {
-  name: "GamingContent"
+  name: 'GamingContent',
+  components: {
+    GamingItem
+  },
+  computed: {
+    ...mapGetters(['gamingVideos'])
+  },
+  methods: {
+    ...mapActions(['fetchGaming'])
+  },
+  created() {
+    this.fetchGaming();
+  }
 };
 </script>
 
@@ -38,28 +38,5 @@ export default {
 /* gaming content */
 .col .gaming__content {
   margin-right: 20px;
-}
-
-.gaming__content img {
-  max-width: 120px;
-  margin-right: 10px;
-}
-
-.item__gaming {
-  padding: 0;
-  display: flex;
-}
-
-.gaming__content li {
-  margin-bottom: 10px;
-  background-color: #363140;
-  border: 1px solid #0c0e1f;
-  border-radius: 10px;
-  padding: 5px;
-}
-
-.gaming__content h3 {
-  font-size: 70%;
-  color: #d2cdd3;
 }
 </style>
