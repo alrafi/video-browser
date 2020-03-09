9fdd8093616adaf274cea90dcc2ab802cfd05cb3
alrafi <hafisalrafi@gmail.com>
2020-03-09 12:58:32 +0700

    add search result item with v-for

--- src/components/SearchResult.vue
+++ src/components/SearchResult.vue
@@ -1,118 +1,20 @@
 <template>
   <div class="search__result">
-    <ul>
-      <li>
-        <div class="image__container">
-          <img src="../assets/thumbnail.webp" alt="thumbnail" />
-        </div>
-        <div class="info__content">
-          <h3>lofi hip hop radio - beats to relax/study to</h3>
-          <p>chilledcow music</p>
-          <p>
-            Thank you for listening, I hope you will have a good time
-            here :) Listen to the playlist on Spotify, Apple music and
-            more
-          </p>
-        </div>
-      </li>
-      <li>
-        <div class="image__container">
-          <img src="../assets/thumbnail.webp" alt="thumbnail" />
-        </div>
-        <div class="info__content">
-          <h3>lofi hip hop radio - beats to relax/study to</h3>
-          <p>chilledcow music</p>
-          <p>
-            Thank you for listening, I hope you will have a good time
-            here :) Listen to the playlist on Spotify, Apple music and
-            more
-          </p>
-        </div>
-      </li>
-      <li>
-        <div class="image__container">
-          <img src="../assets/thumbnail.webp" alt="thumbnail" />
-        </div>
-        <div class="info__content">
-          <h3>lofi hip hop radio - beats to relax/study to</h3>
-          <p>chilledcow music</p>
-          <p>
-            Thank you for listening, I hope you will have a good time
-            here :) Listen to the playlist on Spotify, Apple music and
-            more
-          </p>
-        </div>
-      </li>
-      <li>
-        <div class="image__container">
-          <img src="../assets/thumbnail.webp" alt="thumbnail" />
-        </div>
-        <div class="info__content">
-          <h3>lofi hip hop radio - beats to relax/study to</h3>
-          <p>chilledcow music</p>
-          <p>
-            Thank you for listening, I hope you will have a good time
-            here :) Listen to the playlist on Spotify, Apple music and
-            more
-          </p>
-        </div>
-      </li>
-      <li>
-        <div class="image__container">
-          <img src="../assets/thumbnail.webp" alt="thumbnail" />
-        </div>
-        <div class="info__content">
-          <h3>lofi hip hop radio - beats to relax/study to</h3>
-          <p>chilledcow music</p>
-          <p>
-            Thank you for listening, I hope you will have a good time
-            here :) Listen to the playlist on Spotify, Apple music and
-            more
-          </p>
-        </div>
-      </li>
+    <ul v-for="i in 10" :key="i">
+      <SearchResultItem></SearchResultItem>
     </ul>
   </div>
 </template>
 
 <script>
+import SearchResultItem from './SearchResultItem';
+
 export default {
-  name: "SearchResult"
+  name: 'SearchResult',
+  components: {
+    SearchResultItem
+  }
 };
 </script>
 
-<style>
-/* search result */
-.search__result {
-  margin-top: 30px;
-}
-
-.search__result ul {
-  list-style: none;
-}
-
-.search__result li {
-  display: flex;
-  width: 80%;
-  margin-bottom: 15px;
-}
-
-.info__content h3 {
-  font-size: 120%;
-  margin-bottom: 5px;
-}
-
-.info__content p {
-  font-size: 70%;
-  margin-bottom: 5px;
-}
-
-.image__container {
-  width: 200px;
-  margin-right: 15px;
-}
-
-.image__container img {
-  width: 100%;
-}
-</style>
+<style></style>
