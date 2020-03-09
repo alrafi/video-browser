f89b2ab721708bc2ca643b6f917393f3dfa53caa
alrafi <hafisalrafi@gmail.com>
2020-03-09 10:36:29 +0700

    update serachbar styling

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,30 +1,60 @@
 <template>
   <div>
-    <form @submit="onSubmit">
-      <input />
-      <button type="submit">Search</button>
+    <form class="form__search">
+      <button type="submit">
+        <i class="fas fa-search"></i>
+      </button>
+      <input type="text" placeholder="Search..." />
     </form>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'SearchBar',
+  name: "SearchBar",
   methods: {
-    onSubmit(event) {
-      event.preventDefault();
-      this.$emit('termSubmit', event.target.children[0].value);
-    }
+    // onSubmit(event) {
+    //   event.preventDefault();
+    //   this.$emit('termSubmit', event.target.children[0].value);
+    // }
   }
 };
 </script>
 
-<style scoped>
-div {
-  text-align: center;
-  margin: 20px;
-}
-input {
+<style>
+.form__search {
+  border: solid 1px #7d6e73;
+  border-radius: 50px;
   width: 75%;
+  display: flex;
+  flex-direction: row;
+  flex: 1;
+  padding: 2px 10px;
+}
+
+.form__search input {
+  background-color: transparent;
+  border: none;
+  width: 100%;
+  color: #7d6e73;
+  font-size: 80%;
+}
+
+.form__search input:focus,
+.form__search button:focus {
+  border: none;
+  outline: none;
+}
+
+.form__search button {
+  background-color: transparent;
+  border: none;
+  margin-right: 10px;
+  display: flex;
+}
+
+.form__search i {
+  color: #7d6e73;
+  font-size: 15px;
 }
 </style>
