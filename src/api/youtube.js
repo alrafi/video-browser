7280c06293baffdb0abdef9db8ab374965a94a47
alrafi <hafisalrafi@gmail.com>
2020-03-09 16:38:53 +0700

    add fetchContent method

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,14 +1,25 @@
 import axios from 'axios';
 
-const ROOT_URL = 'https://www.googleapis.com/youtube/v3/search';
+const ROOT_URL = 'https://www.googleapis.com/youtube/v3';
+// 'https://www.googleapis.com/youtube/v3/videos'
 
 export default {
   fetchVideosBySearch: searchTerm => {
-    return axios.get(`${ROOT_URL}`, {
+    return axios.get(`${ROOT_URL}/search`, {
       params: {
         key: process.env.VUE_APP_API_KEY,
         part: 'snippet',
         q: searchTerm
+      }
+    });
+  },
+  fetchTrendingVideos: () => {
+    return axios.get(`${ROOT_URL}/videos`, {
+      params: {
+        key: process.env.VUE_APP_API_KEY,
+        part: 'snippet, statistics',
+        chart: 'mostPopular',
+        regionCode: 'ID'
       }
     });
   }
