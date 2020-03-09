ca2356e6dd51e5e0976bcf715349e692f9a41c2a
alrafi <hafisalrafi@gmail.com>
2020-03-09 17:01:22 +0700

    call global mutation when select content

--- src/store/modules/content.js
+++ src/store/modules/content.js
@@ -1,17 +1,25 @@
 import api from '../../api/youtube';
+import { router } from '../../main';
 
 const state = {
-  trending: []
+  trending: [],
+  selectedContent: null
 };
 
 const getters = {
-  trendingVideos: state => state.trending
+  trendingVideos: state => state.trending,
+  getSelectedContent: state => state.selectedContent
 };
 
 const actions = {
   async fetchContent({ commit }) {
     const response = await api.fetchTrendingVideos();
     commit('setTrending', response.data.items);
+  },
+  selectContent({ commit }, video) {
+    commit('setSelectedVideo', video);
+    router.push('/play');
+    commit('setKind', video.kind, { root: true });
   }
 };
 
@@ -19,6 +27,9 @@ const mutations = {
   setTrending: (state, videos) => {
     state.trending = videos;
     console.log(state.trending);
+  },
+  setSelectedContent: (state, video) => {
+    state.selectedContent = video;
   }
 };
 
