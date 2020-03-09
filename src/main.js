55a5914c8c82cabaacdf3319a3d2281f80b37908
alrafi <hafisalrafi@gmail.com>
2020-03-09 12:08:30 +0700

    setup router

--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './App';
+import Content from './components/Content';
+import SearchResult from './components/SearchResult';
+
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      component: Content
+    },
+    {
+      path: '/search',
+      component: SearchResult
+    }
+  ]
+});
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app');
