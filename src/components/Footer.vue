140472a0fb11bc587003388a84aaaf4fe0e41cb8
alrafi <hafisalrafi@gmail.com>
2020-03-10 21:57:06 +0700

    add footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer>
+    <p>Made with ❤️ by hafisalrafi</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+};
+</script>
+
+<style scoped>
+footer p {
+  text-align: center;
+  font-size: 80%;
+}
+</style>
